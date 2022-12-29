e7350cc60e9c72ff820626e3171669514c38e33f
ethan04tsai <120005618+ethan04tsai@users.noreply.github.com>
2022-12-29 16:43:50 +0800

    Create main.c

---
+++ QZ2/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int rows = 5;
+void print_space(int r, int rows){
+    
+}
+
+void print_stars(int r){
+    
+}
+
+
+
+int main(){
+    int rows = 5;
+    for()
+    printf("Hello World");
+
+    return 0;
+}
