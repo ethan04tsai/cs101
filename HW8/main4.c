320e6ba04e9d5bd291c1f00999fcecc3dc5956cf
ethan04tsai <120005618+ethan04tsai@users.noreply.github.com>
2023-01-04 12:30:13 +0800

    Create main4.c

---
+++ HW8/main4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int round_func(float f){
+    return (int) (f + 0.5f);
+}
+
+int main() {
+    float i = 9.2344567789;
+    printf("%d", round_func(i));
+    return 0;
+}
