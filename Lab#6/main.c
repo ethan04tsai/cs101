c67522c3fc73caec71e16afeb448ab8ca05d15e1
ethan04tsai <120005618+ethan04tsai@users.noreply.github.com>
2023-04-13 16:17:16 +0800

    Update main.c

--- Lab#6/main.c
+++ Lab#6/main.c
@@ -1,48 +1,22 @@
 #include <stdio.h>
 
-void multi(int i, int j) {
-    printf("%d%d=%d ", i, j, ij);
-    if (j == 9) {
+void print_table(int i, int j)
+{
+    if (i > 9) {
+        return;
+    }
+    if (j > 9) {
         printf("\n");
-        return;
-    } else {
-        multi(i, j+1);
-    }
-}
-
-void table(int i) {
-    if (i == 10) {
+        print_table(i+1, 1);
         return;
     }
-    multi(i, 1);
-    table(i+1);
+    printf("%d*%d=%d\t", i, j, i*j);
+    print_table(i, j+1);
 }
 
-int main() {
-    table(1);
-    return 0;
-}
-#include <stdio.h>
-
-void multi(int i, int j) {
-    printf("%d%d=%d ", i, j, ij);
-    if (j == 9) {
-        printf("\n");
-        return;
-    } else {
-        multi(i, j+1);
-    }
-}
-
-void table(int i) {
-    if (i == 10) {
-        return;
-    }
-    multi(i, 1);
-    table(i+1);
-}
-
-int main() {
-    table(1);
+int main()
+{
+    
+    print_table(1, 1);
     return 0;
 }
